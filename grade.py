177226ec9edfffaaaefb983fb914006020520eba
Noman <nmazhar911@gmail.com>
2025-12-13 15:07:03 +0500

    updating langchain requirements

--- grade.py
+++ grade.py
@@ -1,5 +1,5 @@
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.output_parsers import PydanticOutputParser
+from langchain_core.output_parsers import PydanticOutputParser
 from pydantic import BaseModel, Field
 from typing import List
 from typing import Dict
